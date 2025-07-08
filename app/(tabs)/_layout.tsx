f4e2d009e562aaaa5f56945b51ae82632e417c6c
Camave <monnier.camille06@gmail.com>
2025-07-07 20:45:34 -0400

    première version
    juste une page de connexion

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,24 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return(
+    <Tabs screenOptions={{tabBarActiveTintColor: "tomato"}}>
+      <Tabs.Screen 
+      name="index" 
+      options={{
+        title: "Home ", 
+        tabBarIcon: ({color, focused}) => {
+          return focused ? (
+          <Entypo name="home" size={24} color={color} /> 
+          ):(
+          <AntDesign name="home" size={24} color="black" />
+          )
+        }
+      }}
+    />
+      <Tabs.Screen name="login" options={{title: "Settings"}}/>
+    </Tabs> 
+  );
+}
